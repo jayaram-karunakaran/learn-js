150701936178a3405a04a252712625b9e47752fe
Jayaram Karunakaran <126938644+jayaram-karunakaran@users.noreply.github.com>
2023-05-10 16:47:53 +0530

    Add files via upload

---
+++ logics/valid-parentheses.ts
@@ -0,0 +1,24 @@
+// Check Valid Parenthesis using recursion function
+
+let pair = { "{": "}", "[": "]", "(": ")" };
+
+function check(str) {
+  if (str.length) {
+    console.log(str);
+    const isFirstAndLastPairMatch = Boolean(str[str.length - 1] && (pair[str[0]] == str[str.length - 1]));
+    const nextPairMatch = Boolean(str[1] && (pair[str[0]] == str[1]));
+    const lastPairMatch = Boolean(str[str.length - 1] && (pair[str[str.length - 2]] == str[str.length - 1]));
+
+    if (str.length > 1 && (isFirstAndLastPairMatch || nextPairMatch || lastPairMatch)) {
+      if (isFirstAndLastPairMatch) str = str.slice(1, str.length - 1);
+      if (nextPairMatch) str = str.slice(2, str.length);
+      if (lastPairMatch) str = str.slice(0, str.length - 2);
+      check(str);
+    }
+    else console.log("Invalid String");
+  }
+  else console.log("Valid String");
+}
+
+
+check('{[]}');
